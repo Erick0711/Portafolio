440884088571ff9c5614e22357d56d12fe98118a
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-05-29 18:35:12 -0400

    Creacion de carpeta js y archivos para animacion de cohete + pantalla de carga + header response

---
+++ js/carga.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+    $("#contenedor-carga").fadeOut();
+    $("body").removeClass("hidden")
+}
---
+++ js/cohete.js
@@ -0,0 +1,10 @@
+let cohete = document.getElementById("cohete")
+let logCohete = document.getElementById("logo")
+
+cohete.addEventListener("click", function(){
+    cohete.classList.toggle("movimiento")
+})
+
+window.onload = function(){
+    let cohete = document.getElementById("cohete")
+}
