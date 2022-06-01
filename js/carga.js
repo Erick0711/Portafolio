1097e1eabbb1b5efb1f9264b4172fb5c1266d11c
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-06-01 16:50:47 -0400

    SE MODIFICO EL SVG PARA COLOCAR 3 ANIMACIONES MAS, SE ELIMINO LA BARRA DE PROGRESION PARA COLOCAR OTRA QUE ESTA EN DESARROLLO.

--- js/carga.js
+++ js/carga.js
@@ -2,7 +2,17 @@ window.onload = function carga(){
     $("#contenedor-carga").fadeOut();
     $("body").removeClass("hidden")
 }
+const numero = document.querySelector(".numero");
+let contador = 0;
 
+setInterval(()=>{
+    if (contador === 80) {
+        clearInterval();
+    }else{
+        contador += 1;
+        numero.textContent = contador + "%";
+    }
+}, 80)
 
 
 
