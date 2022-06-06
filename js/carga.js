d95ef3f78354b557bdb172369ebbce5e956bd4ac
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-06-06 19:55:48 -0400

    SE ESTABLECIO YA LA BARRA DE PROGRESO HECHO POR MI SIN NINGUN COPIE DE CODIGO, TAMBIEN SE INTRODUCIO UNA NUEVA IMAGEN, SOLO FALTA INTRODUCIR UN POCO DE ORDEN O MOVIMIENTOS AL MOMENTO DE BAJAR CON JS

--- js/carga.js
+++ js/carga.js
@@ -2,117 +2,64 @@ window.onload = function carga(){
     $("#contenedor-carga").fadeOut();
     $("body").removeClass("hidden")
 }
-// const numero = document.querySelector(".numero");
-// let contador = 0;
+const lengHtml = document.querySelector(".html"),
+ lengCss = document.querySelector(".css"),
+ lengBootstrap = document.querySelector(".bootstrap"),
+ lengJs = document.querySelector(".js"),
+ lengPhp = document.querySelector(".carga-php"),
+ lengLaravel = document.querySelector(".laravel");
+// const numero2 = document.querySelector(".numero2");
 
-// setInterval(()=>{
-//     if (contador === 80) {
-//         clearInterval();
-//     }else{
-//         contador += 1;
-//         numero.textContent = contador + "%";
-//     }
-// }, 80)
+let contador = 0;
+let contador2 = 0;
+let contador3 = 0;
+let contador4 = 0;
+let contador5 = 0;
+let contador6 = 0;
 
+setInterval(()=>{
+    if (contador === 85) {
+        clearInterval();
+    }else{
+        contador += 1;
+        lengHtml.textContent = contador + "%";
+    }
 
-// CONTADOR DE PROGRESO MAS AGREGACION DE ESTILOS
-let progressBar = document.querySelector(".circular-progress");
-let valueContainer = document.querySelector(".value-container");
+    if(contador2 === 85){
+      clearInterval();
+    }else{
+      contador2 += 1;
+      lengCss.textContent = contador2 + "%";
+    }
 
-let progressValue = 0;
-let progressEndValue = 80;
-let speed = 50;
+    if(contador3 === 70){
+      clearInterval();
+    }else{
+      contador3 += 1;
+      lengBootstrap.textContent = contador3 + "%";
+    }
 
-let progress = setInterval(() => {
-  progressValue++;
-  valueContainer.textContent = `${progressValue}%`;
-  progressBar.style.background = `conic-gradient(
-      #fc490b ${progressValue * 3.6}deg,
-      #cadcff ${progressValue * 3.6}deg
-  )`;
-  if (progressValue == progressEndValue) {
-    clearInterval(progress);
-  }
-}, speed);
+    if(contador4 === 40){
+      clearInterval();
+    }else{
+      contador4 += 1;
+      lengJs.textContent = contador4 + "%";
+    }
+    
+    if(contador5 === 45){
+      clearInterval();
+    }else{
+      contador5 += 1;
+      lengPhp.textContent = contador5 + "%";
+    }
 
-
-let progressBar2 = document.querySelector(".circular-progress2");
-let valueContainer2 = document.querySelector(".value-container2");
-
-let progressValue2 = 0;
-let progressEndValue2 = 80;
-let speed2 = 50;
-
-let progress2 = setInterval(() => {
-  progressValue2++;
-  valueContainer2.textContent = `${progressValue2}%`;
-  progressBar2.style.background = `conic-gradient(
-      #2196f3 ${progressValue2 * 3.6}deg,
-      #cadcff ${progressValue2 * 3.6}deg
-  )`;
-  if (progressValue2 == progressEndValue2) {
-    clearInterval(progress2);
-  }
-}, speed2);
-
-
-let progressBar3= document.querySelector(".circular-progress3");
-let valueContainer3 = document.querySelector(".value-container3");
-
-let progressValue3 = 0;
-let progressEndValue3 = 70;
-let speed3 = 50;
-
-let progress3 = setInterval(() => {
-  progressValue3++;
-  valueContainer3.textContent = `${progressValue3}%`;
-  progressBar3.style.background = `conic-gradient(
-      #5e3b7f ${progressValue3 * 3.6}deg,
-      #cadcff ${progressValue3 * 3.6}deg
-  )`;
-  if (progressValue3 == progressEndValue3) {
-    clearInterval(progress3);
-  }
-}, speed3);
-
-let progressBar4= document.querySelector(".circular-progress4");
-let valueContainer4 = document.querySelector(".value-container4");
-
-let progressValue4 = 0;
-let progressEndValue4 = 30;
-let speed4 = 50;
-
-let progress4 = setInterval(() => {
-  progressValue4++;
-  valueContainer4.textContent = `${progressValue4}%`;
-  progressBar4.style.background = `conic-gradient(
-      #ffdf00 ${progressValue4 * 3.6}deg,
-      #cadcff ${progressValue4 * 3.6}deg
-  )`;
-  if (progressValue4 == progressEndValue4) {
-    clearInterval(progress4);
-  }
-}, speed4);
-
-
-let progressBar5= document.querySelector(".circular-progress5");
-let valueContainer5 = document.querySelector(".value-container5");
-
-let progressValue5 = 0;
-let progressEndValue5 = 40;
-let speed5 = 50;
-
-let progress5 = setInterval(() => {
-  progressValue5++;
-  valueContainer5.textContent = `${progressValue5}%`;
-  progressBar5.style.background = `conic-gradient(
-      #000 ${progressValue5 * 3.6}deg,
-      #cadcff ${progressValue5 * 3.6}deg
-  )`;
-  if (progressValue5 == progressEndValue5) {
-    clearInterval(progress5);
-  }
-}, speed5);
+    if(contador6 === 30){
+      clearInterval();
+    }else{
+      contador6 += 1;
+      lengLaravel.textContent = contador6 + "%";
+    }
+}, 90)
 
 
 
