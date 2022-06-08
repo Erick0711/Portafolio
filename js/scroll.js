a715c6b86f0bb0bf2e135c926158ec88eda0d077
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-06-08 17:50:59 -0400

    SE AGREGO js PARA EL SCROLL Y EL ICONO HACIA ARRIBA

--- js/scroll.js
+++ js/scroll.js
@@ -10,10 +10,21 @@ function mostrarScroll(){
 
         let alturaAnimado = animado[i].offsetTop;
 
-        if(alturaAnimado - 550 < scrollTop){
+        if(alturaAnimado - 450 < scrollTop){
             animado[i].style.opacity = 1;
+            animado[i].classList.add("mostrarElemento");
         }
     }
 }
 
 window.addEventListener("scroll", mostrarScroll);
+
+$(document).ready(function(){ irArriba(); }); //Hacia arriba
+
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
