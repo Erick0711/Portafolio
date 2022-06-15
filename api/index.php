70ead488ef4eb2bf4843263707eb5f5aac604ca4
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-06-15 17:57:13 -0400

    PRUEBA 9 DE ENVIO DE MENSAJE

--- api/index.php
+++ api/index.php
@@ -1,3 +1,22 @@
+<?php
+    $destinatario = 'maicolarteaga0711@gmail.com';
+    // ESTO ES PARA EL CORREO AL QUE SE ENVIARA EL MENSAJE
+
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $correo = $_POST['correo'];
+    $asunto = $_POST['mensaje'];
+
+    // $header = "ENVIADO DESDE EL PORTAFOLIO DE MAICOL ERICK";
+
+    $mensajeCompleto = $asunto ."\nAtentamente:". $nombre. " " . $apellido. $correo;
+    
+    mail($destinatario, "Contacto",$mensajeCompleto);
+
+    header("Location:index.html");
+    // echo "<script>alert('Correo Enviado')</script>";
+    // echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
