1a7d768169b563d29df30be136272d1e7bed7207
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-06-15 17:39:10 -0400

    PRUEBA 7 DE ENVIO DE CORREO CON PHP

--- correos.php
+++ correos.php
@@ -7,12 +7,13 @@
     $correo = $_POST['correo'];
     $asunto = $_POST['mensaje'];
 
-    $header = "ENVIADO DESDE EL PORTAFOLIO DE MAICOL ERICK";
+    // $header = "ENVIADO DESDE EL PORTAFOLIO DE MAICOL ERICK";
 
-    $mensajeCompleto = $asunto ."Atentamente:". $nombre. " " . $apellido. $correo;
+    $mensajeCompleto = $asunto ."\nAtentamente:". $nombre. " " . $apellido. $correo;
     
-    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    mail($destinatario, "Contacto",$mensajeCompleto);
 
-    echo "<script>alert('Correo Enviado')</script>";
-    echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
+    header("Location:index.html");
+    // echo "<script>alert('Correo Enviado')</script>";
+    // echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
 ?>
